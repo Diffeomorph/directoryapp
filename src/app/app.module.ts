a75c175597b60178b48e53130ce68306f5925e81
gf1721 <gf1721@bristol.ac.uk>
2018-01-16 14:53:52 +0000

    firstrouting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ListingsComponent } from './listings/listings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListingsComponent 
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'listings',
+        component: ListingsComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
